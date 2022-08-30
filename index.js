30a29ee69f45150ef967691cee86e97c302be14d
Cindy Qu <cindyqu42@gmail.com>
2022-08-30 12:51:58 -0700

    commit tree 191faaa9fb1e6a1e9a3222d77d852a6453beae0c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if (someValue > 42) {
+        return someValue - 42;
+    } else {
+        return 42 - someValue;
+    }
+}
+
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+
+function distanceTravelledInFeet(beginning, end) {
+    if (beginning < end) {
+        return (end - beginning) * 264;
+    } else {
+        return (beginning - end) * 264;
+    }
+
+}
+
+function calculatesFarePrice(beginning, end) {
+    const distance = distanceTravelledInFeet(beginning, end);
+
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return .02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
